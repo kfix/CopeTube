8e4bfdca79a5bfe2dd21e63e3df56051feebaed0
Joey Korkames <kfix@users.noreply.github.com>
2019-11-26 13:51:03 -0700

    auto-start the demo app

--- app/demo.js
+++ app/demo.js
@@ -4,16 +4,8 @@ console.log(import.meta);
 
 function demo(el) {
 	console.log(el);
-/*
-    try {
-		showDemo(el);
-	} catch(e) {
-		setTimeout(function() { throw e.stack || e; }); // propagate error to console
-		return e;
-	};
-*/
 	showDemo(el);
 }
 
-//demo(document.getElementById('demoRoot'));
+demo(document.getElementById('demoRoot'));
 window.demo = demo;
